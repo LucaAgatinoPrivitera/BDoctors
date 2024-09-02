1b49605c6aa7c7e188dd84bd6c60b38adcedbb58
GuglielmoConsolini <consoliniguglielmo@gmail.com>
2024-09-02 20:52:21 +0200

    nome nella tab : BDoctors

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -7,7 +7,7 @@
 	<meta name="csrf-token" content="{{ csrf_token() }}">
 
 	{{-- <title>{{ config('app.name', 'Laravel') }}</title> --}}
-	<title>@yield('title', config('app.name', 'Laravel'))</title>
+	<title>@yield('title', config('app.name', 'BDoctors'))</title>
 
 	<!-- Favicon -->
 	<link rel="icon" href="{{ asset('favicon.ico') }}" type="image/x-icon">
