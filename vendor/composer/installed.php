76f3ba0c9affc83e78e422b5079f523f0ffa48dc
Samudra-Ekanayake <samudra2724@gmail.com>
2024-09-09 11:15:17 +0200

    riclonato repo

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -1,9 +1,9 @@
 <?php return array(
     'root' => array(
         'name' => 'laravel/laravel',
-        'pretty_version' => '1.0.0+no-version-set',
-        'version' => '1.0.0.0',
-        'reference' => null,
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '99d71ff859cf42cbe42ab5a871d31c7932c2a12b',
         'type' => 'project',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -425,9 +425,9 @@
             'dev_requirement' => false,
         ),
         'laravel/laravel' => array(
-            'pretty_version' => '1.0.0+no-version-set',
-            'version' => '1.0.0.0',
-            'reference' => null,
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '99d71ff859cf42cbe42ab5a871d31c7932c2a12b',
             'type' => 'project',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
