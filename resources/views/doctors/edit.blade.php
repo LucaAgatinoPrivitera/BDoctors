4ec32a2ded442c32531fbc6ce23676d453847de7
GuglielmoConsolini <consoliniguglielmo@gmail.com>
2024-09-10 12:59:11 +0200

    rotta aggiustata

--- resources/views/doctors/edit.blade.php
+++ resources/views/doctors/edit.blade.php
@@ -1,103 +1,104 @@
 @extends('layouts.app')
 
-@section('title', 'Modifica il tuo profilo ')
+@section('title', 'Modifica il tuo profilo')
 
 @section('content')
-	<div class="container">
-		<div class="row">
-			<div class="col-12">
-				@if ($errors->any())
-					<div class="alert alert-danger">
-						<ul>
-							@foreach ($errors->all() as $error)
-								<li>{{ $error }}</li>
-							@endforeach
-						</ul>
-					</div>
-				@endif
-			</div>
-		</div>
+    <div class="container">
+        <div class="row">
+            <div class="col-12">
+                @if ($errors->any())
+                    <div class="alert alert-danger">
+                        <ul class="mb-0">
+                            @foreach ($errors->all() as $error)
+                                <li>{{ $error }}</li>
+                            @endforeach
+                        </ul>
+                    </div>
+                @endif
+            </div>
+        </div>
 
-		<div class="row">
-			<div class="col-12">
+        <div class="row">
+            <div class="col-12">
+                <form method="POST" action="{{ route('doctors.update', $doctor) }}" enctype="multipart/form-data">
+                    @csrf
+                    @method('PUT')
 
-				{{-- Questo form non carica una generica rotta "store" ma ha bisogno dell'id del gioco da aggiornare --}}
-				<form method="POST" action="{{ route('doctors.update', $doctor) }}" enctype="multipart/form-data">
-					@csrf
-					@method('PUT')
-					<div class="mb-3">
-						<h3 class="form-label">Nome</h3>
-						<input type="text" class="form-control" name="name" required value="{{ $doctor->user->name }}">
-						@error('name')
-							<div>{{ $message }}</div>
-						@enderror
-					</div>
-					<div class="mb-3">
-						<h3 class="form-label">Cognome</h3>
-						<input type="text" class="form-control" name="surname" required value="{{ $doctor->surname }}">
-						@error('surname')
-							<div>{{ $message }}</div>
-						@enderror
-					</div>
-					<div class="mb-3">
-						<h3 class="form-label">Descrizione</h3>
-						<textarea type="text" class="form-control" name="address" required>{{ $doctor->address }}</textarea>
-					</div>
+                    <div class="mb-3">
+                        <label class="form-label" for="name">Nome</label>
+                        <input type="text" class="form-control" id="name" name="name" required value="{{ old('name', $doctor->user->name) }}">
+                        @error('name')
+                            <div class="text-danger">{{ $message }}</div>
+                        @enderror
+                    </div>
 
-					<div class="mb-3">
-						<h3 class="form-label">Telefono</h3>
-						<textarea type="text" class="form-control" name="phone" required>{{ $doctor->phone }}</textarea>
-					</div>
+                    <div class="mb-3">
+                        <label class="form-label" for="surname">Cognome</label>
+                        <input type="text" class="form-control" id="surname" name="surname" required value="{{ old('surname', $doctor->surname) }}">
+                        @error('surname')
+                            <div class="text-danger">{{ $message }}</div>
+                        @enderror
+                    </div>
 
-					<div class="mb-3">
-						<h3 class="form-label">Bio</h3>
-						<textarea type="text" class="form-control" name="bio" required>{{ $doctor->bio }}</textarea>
-					</div>
+                    <div class="mb-3">
+                        <label class="form-label" for="address">Descrizione</label>
+                        <textarea class="form-control" id="address" name="address" rows="3" required>{{ old('address', $doctor->address) }}</textarea>
+                    </div>
 
-					<div class="mb-3">
-						<h3 class="form-label">Specializzazioni</h3>
-						<select class="form-control" name="specializations[]" multiple>
-							@foreach ($specializations as $specialization)
-								<option value="{{ $specialization->id }}"
-									{{ in_array($specialization->id, old('specializations', $doctor->specializations->pluck('id')->toArray())) ? 'selected' : '' }}>
-									{{ $specialization->name }}
-								</option>
-							@endforeach
-						</select>
-						@error('specializations')
-							<div>{{ $message }}</div>
-						@enderror
-					</div>
+                    <div class="mb-3">
+                        <label class="form-label" for="phone">Telefono</label>
+                        <textarea class="form-control" id="phone" name="phone" rows="1" required>{{ old('phone', $doctor->phone) }}</textarea>
+                    </div>
 
-					<div class="form-group">
-						<label for="pic">Immagine</label>
-						@if ($doctor->pic)
-							<div>
-								<img src="{{ Storage::url($doctor->pic) }}" alt="Immagine del medico" style="width: 100px; height: auto;">
-								<p>Immagine attuale</p>
-							</div>
-						@endif
-						<input class="btn" type="file" name="pic" class="form-control">
-					</div>
+                    <div class="mb-3">
+                        <label class="form-label" for="bio">Bio</label>
+                        <textarea class="form-control" id="bio" name="bio" rows="4" required>{{ old('bio', $doctor->bio) }}</textarea>
+                    </div>
 
-					<div class="form-group">
-						<label for="cv">CV</label>
-						@if ($doctor->cv)
-							<div>
-								<a href="{{ Storage::url($doctor->cv) }}" target="_blank">Visualizza il CV attuale</a>
-								<p>CV attuale</p>
-							</div>
-						@endif
-						<input class="btn" type="file" name="cv" class="form-control">
-						@error('cv')
-							<div>{{ $message }}</div>
-						@enderror
-					</div>
+                    <div class="mb-3">
+                        <label class="form-label" for="specializations">Specializzazioni</label>
+                        <select class="form-select" id="specializations" name="specializations[]" multiple>
+                            @foreach ($specializations as $specialization)
+                                <option value="{{ $specialization->id }}"
+                                    {{ in_array($specialization->id, old('specializations', $doctor->specializations->pluck('id')->toArray())) ? 'selected' : '' }}>
+                                    {{ $specialization->name }}
+                                </option>
+                            @endforeach
+                        </select>
+                        @error('specializations')
+                            <div class="text-danger">{{ $message }}</div>
+                        @enderror
+                    </div>
 
-					<button type="submit" class="btn btn-primary">Submit</button>
-				</form>
+                    <div class="mb-4">
+                        <label class="form-label" for="pic">Immagine</label>
+                        @if ($doctor->pic)
+                            <div class="mb-2">
+                                <img src="{{ Storage::url($doctor->pic) }}" alt="Immagine del medico" class="img-thumbnail" style="max-width: 150px;">
+                                <p class="mt-2">Immagine attuale</p>
+                            </div>
+                        @endif
+                        <input type="file" class="form-control" id="pic" name="pic">
+                    </div>
 
-			</div>
-		</div>
-	</div>
+                    <div class="mb-4">
+                        <label class="form-label" for="cv">CV</label>
+                        @if ($doctor->cv)
+                            <div class="mb-2">
+                                <a href="{{ Storage::url($doctor->cv) }}" target="_blank" class="btn btn-link">Visualizza il CV attuale</a>
+                                <p class="mt-2">CV attuale</p>
+                            </div>
+                        @endif
+                        <input type="file" class="form-control" id="cv" name="cv">
+                        @error('cv')
+                            <div class="text-danger">{{ $message }}</div>
+                        @enderror
+                    </div>
+
+                    <button type="submit" class="btn btn-primary">Aggiorna</button>
+                </form>
+            </div>
+        </div>
+    </div>
 @endsection
+
