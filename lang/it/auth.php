44c5903bb69e3eb7b07c0873f4f515bf74d10a3a
Your Name <you@example.com>
2024-09-15 21:26:43 +0200

    validazioni errori in italiano

---
+++ lang/it/auth.php
@@ -0,0 +1,21 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Linee di lingua per l'autenticazione
+    |--------------------------------------------------------------------------
+    |
+    | Le seguenti linee di lingua sono utilizzate durante l'autenticazione
+    | per vari messaggi che dobbiamo mostrare all'utente. Sei libero di
+    | modificare queste linee secondo le esigenze della tua applicazione.
+    |
+    */
+
+    'failed' => 'Queste credenziali non corrispondono ai nostri record.',
+    'password' => 'La password fornita è errata.',
+    'throttle' => 'Troppi tentativi di accesso. Per favore riprova tra :seconds secondi.',
+    
+];
+
---
+++ lang/it/passwords.php
@@ -0,0 +1,22 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Linee di lingua per il Reset della Password
+    |--------------------------------------------------------------------------
+    |
+    | Le seguenti righe sono le traduzioni predefinite che corrispondono ai
+    | motivi forniti dal broker delle password per il fallimento di un
+    | tentativo di aggiornamento della password, come un token non valido.
+    |
+    */
+
+    'reset' => 'La tua password è stata reimpostata.',
+    'sent' => 'Abbiamo inviato via email il link per il reset della password.',
+    'throttled' => 'Attendi prima di riprovare.',
+    'token' => 'Questo token di reset della password non è valido.',
+    'user' => "Non riusciamo a trovare un utente con quell'indirizzo email.",
+
+];
